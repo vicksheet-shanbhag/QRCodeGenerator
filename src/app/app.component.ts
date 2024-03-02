e1470cc9dd5b12f805c7026797fda54dd825ca9b
vicksheet-shanbhag <43930886+vicksheet-shanbhag@users.noreply.github.com>
2024-03-02 22:59:04 +0530

    Initial code commit
    Changes with QR code generator and download QR code image.

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { FormControl , FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { SafeUrl } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'qrcodegenerator';
+  qrData = "";
+  message = "Please enter some content to generate QR code";
+  qrCodeDownloadLink: SafeUrl = '';
+  constructor(public snackBar:MatSnackBar) {}
+  
+  generateQR = new FormGroup({
+    qrcodeString: new FormControl('',Validators.required)
+  });
+  generate(){
+    if(this.generateQR.controls['qrcodeString'].value != ""){
+      this.qrData = this.generateQR.value['qrcodeString'];
+    }
+
+  }
+
+  onChangeURL(url: SafeUrl){
+    this.qrCodeDownloadLink= url;
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { QRCodeModule } from 'angularx-qrcode';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    QRCodeModule,
+    ReactiveFormsModule,
+    MatSnackBarModule,
+    BrowserAnimationsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
